0b88c0f6d0370cedbfdc4d3d6590e24663acaa13
Henry Marquardt <hmarquardt@berkeley.edu>
2024-02-25 17:46:53 -0500

    Revert "merging branch letters to main"
    This reverts commit 91fecfe3ed9b12249cc48e29d9c64a50f3aaa918, reversing
    changes made to 7bedb9b3072bdbaff5815237cbdd12d7cec8f082.

--- Project1/torchnn.py
+++ Project1/torchnn.py
@@ -1,17 +1,43 @@
 # pytorch neural networks
 
+import subprocess
+import time
+import sys
 from torch import nn, save, load
 # optimizer
 from torch.optim import Adam
-# to load datasets
+# to load datsets
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor, Compose
 
+# try:
+#     import subprocess
+#     import time
+#     import sys
+#     from torch import nn, save, load
+#     # optimizer
+#     from torch.optim import Adam
+#     # to load datsets
+#     from torch.utils.data import DataLoader
+#     from torchvision import datasets
+#     from torchvision.transforms import ToTensor, Compose
+# except ModuleNotFoundError as e:
+#     subprocess.Popen([f'{sys.executable} -m pip install torchvision'], shell=True)
+#     subprocess.Popen([f'{sys.executable} -m pip install torch'], shell=True)
+#     from torchvision import datasets
+#     from torchvision.transforms import ToTensor, Compose
+#     from torch import nn, save, load
+#     from torch.optim import Adam
+#     from torch.utils.data import DataLoader    
+#     time.sleep(90)
+
+
 # Used to load model after initial training
 import torch
 from PIL import Image
-import os, sys, datetime
+import os, sys
+
 
 # image classifier neural network
 class ImageClassifier(nn.Module):
@@ -25,7 +51,7 @@ class ImageClassifier(nn.Module):
             nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = (3,3)),
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(in_features = 64*(28-6)*(28-6), out_features =  62)  # Changed output layer size to 62 for EMNIST
+            nn.Linear(in_features = 64*(28-6)*(28-6), out_features =  10)
         )    
 
     def forward(self, x):
@@ -33,58 +59,54 @@ class ImageClassifier(nn.Module):
 
 def train_nn():
     # import dataset
-    train = datasets.EMNIST(root = "data", download=True, train=True, transform=ToTensor(), split='letters')
-    dataset = DataLoader(dataset=train, batch_size=32) # 62 classes: digits, uppercase and lowercase letters
+    train = datasets.MNIST(root = "data", download=True, train=True, transform=ToTensor())
+    dataset = DataLoader(dataset=train, batch_size=32) # 10 classes, one for each digit 0 through 9
 
-    # Local CPU training
+    # local cpu training ~ 15 minutes
     print("\n\nBeginning the training process...")
-    for epoch in range(2):
-        print(f"Current time: {datetime.datetime.now().time()}")
-
+    for epoch in range(10):
         for batch in dataset:
-            X, y = batch
-            X, y = X.to('cpu'), y.to('cpu')
-            # Make prediction
+            X,y = batch
+            X,y = X.to('cpu'), y.to('cpu')
+            # make prediction
             yhat = clf(X)
             loss = loss_fn(yhat, y)
 
-            # Apply backpropagation
-            opt.zero_grad() # Zero out gradient
-            loss.backward() # Calculate gradients
+            # Apply backpro
+            opt.zero_grad() # zero out gradient
+            loss.backward() # calculate gradients
             opt.step()
 
         print(f"Epoch: {epoch} loss is {loss.item()}")
 
-
     with open('model_state.pt', 'wb') as f:
         torch.save(clf.state_dict(), f)
 
-# Training function
+# training function
 if __name__ == "__main__":
-    # Create instance of the neural network, loss, optimizer
+    # create instance of the neural network, loss, optimizer
     clf = ImageClassifier().to('cpu')
     opt = Adam(clf.parameters(), lr=1e-3)
     loss_fn = nn.CrossEntropyLoss()
 
-    # Train the model if the state doesn't exist:
-    retrain_model = False
-    if retrain_model:
+    # train the model if the state doesn't exist:
+    if not(os.path.exists('model_state.pt')):
         train_nn()
 
-    # Load the stored PyTorch NN
+    # load the stored pytorch nn
     with open('model_state.pt', 'rb') as f:
         clf.load_state_dict(torch.load(f))
 
     # Load the RGB image
     if len(sys.argv) > 1:
-        img = Image.open(f"data/{sys.argv[1]}.jpg")
+        img = Image.open(f"{sys.argv[1]}.jpg")
     else:
-        # Default to image 1 if no arg is given
-        img = Image.open('data/img_1.jpg')
+        # default to image 1 if no arg is given
+        img = Image.open('img_1.jpg')
 
     # Convert the image to grayscale
     img = img.convert(mode='L')
-    # Resize the image to the appropriate size (based on the MNIST training set)
+    # resize the image to the appropriate size (based on the MNIST training set)
     img = img.resize((28, 28))
 
     img_tensor = ToTensor()(img).unsqueeze(0).to('cpu')
